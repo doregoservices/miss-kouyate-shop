8ab166fae8eb88c5e0b32f3c8d088ef08589595e
doregoservices <doregoservices@gmail.com>
2026-08-20 16:04:55 +0000

    Mise à jour MISS KOUYATE SHOP (correctif code d'accès)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    l'application elle-même est enregistrée en cache, et les données
    (ventes, dépenses…) sont sauvegardées sur l'appareil par l'application.
    Pour mettre à jour l'application : changer le numéro de version ci-dessous. */
-const CACHE = 'miss-kouyate-v18';
+const CACHE = 'miss-kouyate-v19';
 const ASSETS = [
   './',
   './index.html',
